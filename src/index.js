a07d8f104068663907f1ac6d9ad9cc5e1bc0d7a2
ian <ianwisdom90@gmail.com>
2025-04-16 10:33:52 +0300

    added About

--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import Home from "./pages/Home";
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Home />
+    }
+  ]);
+  
+  const root = ReactDOM.createRoot(document.getElementById("root"));
+  root.render(<RouterProvider router={router} />);
 root.render(<Home />);
---
+++ src/pages/Login.js
@@ -0,0 +1,28 @@
+function Login() {
+  return (
+    <>
+    <haeder>
+        {/*save space for NavBar*/}
+    </haeder>
+      <main>
+        <h1>Login</h1>
+        <form>
+          <div>
+            <label for="username">Username: </label>
+            <input id="username" type="text" name="username" placeholder="Username" />
+          </div>
+          <br/>
+          <div>
+            <label for="password">Password: </label>
+            <input id="password" type="password" name="password" placeholder="Password" />
+          </div>
+          <br/>
+          <button type="submit">Submit</button>
+        </form>
+      </main>
+    </>
+  );
+};
+
+export default Login;
+    
